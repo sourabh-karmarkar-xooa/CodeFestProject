29eaf646afeaf543c1b7218996a99231b9c8e602
Seema Deshpande <seema.deshpande@synerzip.com>
2019-11-10 17:59:42 +0530

    Added Login component

--- src/App.js
+++ src/App.js
@@ -2,16 +2,20 @@ import React from 'react';
 import './App.css';
 import { Provider } from "react-redux";
 import {configureStore} from './store';
+import { withRouter,Route } from "react-router";
 import MainComponent from './components/MainComponent';
+import LoginComponent from './components/LoginComponent'
 
 const store = configureStore();
 
 function App() {
   return (
       <Provider store={store}>
-        <MainComponent/>
+      <Route path="/" exact component={LoginComponent} />
+      <Route path="/login"  component={LoginComponent} />
+      <Route path="/stationery" component={MainComponent}/>
       </Provider>
   );
 }
 
-export default App;
+export default withRouter(App);
